f71bd5125e172533cc8e0b07b598e626eeddbb25
adriapedralbes <a23adrestmar@inspedralbes.cat>
2025-02-15 11:03:21 +0100

    canvi ruta invitations

--- backend/app/Http/Controllers/InvitationController.php
+++ backend/app/Http/Controllers/InvitationController.php
@@ -33,7 +33,7 @@ class InvitationController extends Controller
         ]);
 
         // Use frontend URL instead of localhost:8000
-        $invitationLink = 'http://localhost:3000' . '/login?invitation=' . $token;
+        $invitationLink = 'https://api.grupify.cat' . '/login?invitation=' . $token;
 
         return response()->json([
             'message'    => 'Invitación creada correctamente.',
