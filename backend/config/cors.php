4456993099e8fdb7b316a95d87985b8c59a6263d
adriestevezdev <adria.esmarquez@gmail.com>
2025-05-23 12:58:24 +0200

    feat: Añadir soporte CORS para rutas de dashboard y localhost en la configuración de CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'dashboard/*', '*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://grupify.cat', 'https://www.grupify.cat'],
+    'allowed_origins' => ['https://grupify.cat', 'https://www.grupify.cat', 'http://localhost:3000'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
